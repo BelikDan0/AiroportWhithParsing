4b2f3ad95dab214bd1d5979224486c79d1fff174
BelikDan0 <belikdana85@gmail.com>
2025-05-14 20:25:11 +0300

    Airports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         String url ="https://www.aviasales.ru/";
         Airoport airoport=new Airoport(url);
-        airoport.getMapAirport();
+
+        for (Map.Entry<String,String >entry:airoport.getMapAirport().entrySet()){
+            Flight flight =new Flight(entry.getValue());
+            System.out.println("\nВылет из "+entry.getKey());
+            flight.printAllFlight();
+        }
     }
 }
